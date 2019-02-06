469ec891ec8b9bba46c0869f165e3249dea7df5a
John Pena <jpena91@gmail.com>
2019-02-06 02:37:07 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -39,7 +39,7 @@ end
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   times = 0..number_of_times.to_i
-  for item.to_i in times.to_i do 
+  for item in times.to_i do 
     puts phrase
   end
 end
